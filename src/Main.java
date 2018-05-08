0d79272f8c7b842376cd08277fc54bf9d7acbb13
kknaur <kknaur@gmail.com>
2018-05-08 11:52:45 -0700

    Added Main.java with some preliminary code.

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("My First Git Project!");
+		
+		Person p1 = new Person("John", 45, 12345);
+		
+		System.out.println(p1.getName());
+	}
+}
