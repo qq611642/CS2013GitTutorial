c798fd1af4dff44c2b52a9e5f3aff6efbfa76370
JmLin <otawuliao@gmail.com>
2018-05-08 12:19:14 -0700

    Update Main.java
    Change test

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Class!");
-		System.out.println("My Second Git Project!");
+		System.out.println("My Final Git Project!");
 				
 		Person p1 = new Person("John", 45, 12345);
 		
